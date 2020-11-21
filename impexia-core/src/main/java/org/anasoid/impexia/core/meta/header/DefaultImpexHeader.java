7681145a83089914b60ede1e6c60fcc09d8b55a5
anasoid <oufdou.anas@gmail.com>
2020-11-21 12:09:19 +0100

    add header dto and raw validator

---
+++ impexia-core/src/main/java/org/anasoid/impexia/core/meta/header/DefaultImpexHeader.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2020-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * @author : anas
+ * Date :   03-Nov-2020
+ */
+
+package org.anasoid.impexia.core.meta.header;
+
+import org.anasoid.impexia.meta.header.ImpexAction;
+import org.anasoid.impexia.meta.header.ImpexAttribute;
+import org.anasoid.impexia.meta.header.ImpexHeader;
+import org.anasoid.impexia.meta.header.ImpexModifier;
+
+public class DefaultImpexHeader extends ImpexHeader {
+
+  public DefaultImpexHeader(String type, ImpexAction action) {
+    this.type = type;
+    this.action = action;
+  }
+
+  public DefaultImpexHeader addModifier(ImpexModifier modifier) {
+    this.modifiers.add(modifier);
+    return this;
+  }
+
+  public DefaultImpexHeader addAttribute(ImpexAttribute attribute) {
+    this.attributes.add(attribute);
+    return this;
+  }
+
+  public DefaultImpexHeader addRawListner(String rawListner) {
+    this.rawListners.add(rawListner);
+    return this;
+  }
+
+  public DefaultImpexHeader setRawErrorHandler(String rawErrorHandler) {
+    this.rawErrorHandler = rawErrorHandler;
+    return this;
+  }
+}
---
+++ impexia-core/src/main/java/org/anasoid/impexia/core/validators/header/AbstractHeaderValidator.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2020-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * @author : anas
+ * Date :   03-Nov-2020
+ */
+
+package org.anasoid.impexia.core.validators.header;
+
+import java.util.List;
+import org.anasoid.impexia.meta.Mode;
+import org.anasoid.impexia.meta.exceptions.header.ImpexHeaderException;
+import org.anasoid.impexia.meta.header.ImpexAttribute;
+import org.anasoid.impexia.meta.header.ImpexHeader;
+import org.anasoid.impexia.meta.header.ImpexModifier;
+import org.anasoid.impexia.meta.modifier.Level;
+import org.anasoid.impexia.meta.modifier.Modifier;
+
+/** Validate Impex header. */
+public abstract class AbstractHeaderValidator implements HeaderValidator {
+
+  @Override
+  public void validate(ImpexHeader header, Mode mode) throws ImpexHeaderException {
+
+    validateHeader(header, mode);
+    List<ImpexModifier> headerModifiers = header.getModifiers();
+    for (ImpexModifier modifier : headerModifiers) {
+      validateModifier(header, null, modifier, mode, Level.TYPE);
+    }
+
+    List<ImpexAttribute> attributes = header.getAttributes();
+    for (ImpexAttribute attribute : attributes) {
+      validateAttribute(header, attribute, mode, Level.FIELD);
+      for (ImpexModifier modifier : attribute.getModifiers()) {
+        validateModifier(header, attribute, modifier, mode, Level.FIELD);
+      }
+    }
+  }
+
+  /**
+   * Validate attribute.
+   *
+   * @param header header
+   * @param attribute attribute
+   * @param mode mode
+   * @param level level
+   * @return true, function do a check or not.
+   * @throws ImpexHeaderException throw exception if header not valid.
+   */
+  protected abstract boolean validateAttribute(
+      ImpexHeader header, ImpexAttribute attribute, Mode mode, Level level)
+      throws ImpexHeaderException;
+
+  /**
+   * Validate Modifier.
+   *
+   * @param header header
+   * @param attribute attribute
+   * @param modifier modifier
+   * @param mode mode
+   * @param level level
+   * @return true, function do a check or not.
+   * @throws ImpexHeaderException throw exception if header not valid.
+   */
+  protected abstract boolean validateModifier(
+      ImpexHeader header, ImpexAttribute attribute, ImpexModifier modifier, Mode mode, Level level)
+      throws ImpexHeaderException;
+
+  /**
+   * Validate Header.
+   *
+   * @param header header
+   * @param mode mode
+   * @return true, function do a check or not.
+   * @throws ImpexHeaderException throw exception if header not valid.
+   */
+  protected abstract boolean validateHeader(ImpexHeader header, Mode mode)
+      throws ImpexHeaderException;
+
+  protected Modifier getModifier(ImpexModifier modifier) {
+    return Modifier.valueByCode(modifier.getKey());
+  }
+}
