5446e44a68fbf6682a3a279f331f3b3c4e5ea13c
anasoid <oufdou.anas@gmail.com>
2020-12-27 13:01:17 +0100

    init rework validator

---
+++ impexia-core/src/main/java/org/anasoid/impexia/core/validators/header/impl/ValueModifierValidator.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2020-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * @author : anas
+ * Date :   03-Nov-2020
+ */
+
+package org.anasoid.impexia.core.validators.header.impl;
+
+import java.text.MessageFormat;
+import org.anasoid.impexia.core.validators.header.ModifierValidator;
+import org.anasoid.impexia.meta.Mode;
+import org.anasoid.impexia.meta.exceptions.header.AttributeModifierException;
+import org.anasoid.impexia.meta.exceptions.header.ImpexHeaderException;
+import org.anasoid.impexia.meta.header.ImpexAttribute;
+import org.anasoid.impexia.meta.header.ImpexHeader;
+import org.anasoid.impexia.meta.header.ImpexModifier;
+import org.anasoid.impexia.meta.modifier.Level;
+import org.anasoid.impexia.meta.modifier.Modifier;
+import org.anasoid.impexia.meta.modifier.ModifierManager;
+
+/** Default header Validator. */
+public class ValueModifierValidator implements ModifierValidator {
+
+  @Override
+  @SuppressWarnings({"PMD.EmptyCatchBlock"})
+  public boolean validate(
+      ImpexHeader header,
+      ImpexAttribute attribute,
+      ImpexModifier impexModifier,
+      Mode mode,
+      Level level)
+      throws ImpexHeaderException {
+
+    Modifier modifier = null;
+    try {
+      modifier = ModifierManager.getInstance().getValueByCode(impexModifier.getKey());
+    } catch (IllegalArgumentException e) {
+      // nothing
+    }
+
+    if (modifier != null) {
+      validateModifierValues(impexModifier, modifier);
+    }
+    return false;
+  }
+
+  protected void validateModifierValues(ImpexModifier impexModifier, Modifier modifierEnum)
+      throws ImpexHeaderException {
+
+    if (modifierEnum.getValues() != null
+        && !modifierEnum.getValues().contains(impexModifier.getValue())) {
+      throw new AttributeModifierException(
+          MessageFormat.format("Field ({0}) should be boolean ", impexModifier.getKey()));
+    }
+  }
+}
